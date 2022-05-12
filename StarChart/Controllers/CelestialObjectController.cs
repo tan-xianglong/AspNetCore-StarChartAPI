502b54c30115b230a5c94072e818c970a5cf40bd
tan-xianglong <t.xianglong@gmail.com>
2022-05-12 15:40:44 +0800

    create get actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
@@ -13,6 +14,44 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
 		{
             _context = context;
+        }
+
+		[HttpGet("{id:int}", Name = "GetById")]
+		public IActionResult GetById(int id)
+        {
+			var celestialObject = _context.CelestialObjects.Find(id);
+			if (celestialObject == null) return NotFound();
+			celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
+			return Ok(celestialObject);
+        }
+
+		[HttpGet("{name}")]
+		public IActionResult GetByName(string name)
+        {
+			var celestialObjects = _context.CelestialObjects
+				.Where(e => e.Name == name);
+
+			if (celestialObjects == null) return NotFound();
+
+			foreach(var celestialObject in celestialObjects)
+            {
+				celestialObject.Satellites = _context.CelestialObjects
+					.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+			return Ok(celestialObjects);
+        }
+
+		[HttpGet]
+		public IActionResult GetAll()
+        {
+			var celestialObjects = _context.CelestialObjects;
+			if (celestialObjects == null) return NotFound();
+			foreach (var celestialObject in celestialObjects)
+            {
+				celestialObject.Satellites = _context.CelestialObjects
+					.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
+			}
+			return Ok(celestialObjects);
         }
 	}
 }
